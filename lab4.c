540b446c85cc81a91d9c16b361fe92982e65ffe6
Matt Bessler <matthew.bessler@cix.csi.cuny.edu>
2018-04-14 13:01:42 -0400

    update file

--- lab4.c
+++ lab4.c
@@ -3,18 +3,52 @@
 #include<stdlib.h>
 #include<string.h>
 
+int findMaxElement(int A[], int);
+void countingSort(int A[], int sortedA[], int, int);
+
 int main(int argc, char * argv[]){
 	
 	MPI_Init(&argc, &argv);
 	
-	int id, count, i, root = 0;
+	int id, rank, i, root = 0;
+	int size = 10000;
+	int max = 300;
+	int A[size], sortedArray[size], globalArray[size];
 	
 	//The id of the processor
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	//Total number of processors
-	MPI_Comm_size(MPI_COMM_WORLD, &count);
+	MPI_Comm_size(MPI_COMM_WORLD, &rank);
 	
+	//Initialize array
+	if(id == 0){
+		for(i = 0; i < size; i++){
+			A[i] = rand() % 300;
+		}
+	}
 	
+	//The amount of elements to send to each processor
+	int sendCount = size / rank;
+	int recvCount = sendCount;
+	int localA[sendCount], recvSortedArrays[rank];
+	
+	//Evenly scattering the elements across all processors
+	MPI_Scatter(A, sendCount, MPI_INT, localA, recvCount, MPI_INT, root, MPI_COMM_WORLD);
+	
+	//Sort the arrays
+	int localSortedArray[recvCount];
+	countingSort(localA, localSortedArray, max, recvCount);
+	
+	if(rank == 0){
+		
+	}
+	
+	//Gather all sorted arrays
+	MPI_Gather(localSortedArray, recvCount, MPI_INT, recvSortedArrays[rank], recvCount, MPI_INT, root, MPI_COMM_WORLD);
+	
+	if(id == 0){
+		for(i = 0; i 
+	}
 	
 	MPI_Finalize();
 }
@@ -37,7 +71,8 @@ void countingSort(int A[], int sortedA[], int max, int size){
 		C[i] = 0;
 	}
 
-	//Counting the # of elements equal to eachother in A
+	//{95, 1, 37, 27, 27}
+	//Counting the # of elements equal to each other in A
 	for (i = 0; i < size; i++) {
 		C[A[i]] = C[A[i]] + 1;
 	}
