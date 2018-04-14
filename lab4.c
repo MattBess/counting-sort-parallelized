ef1ee42a48ac81cbae308ecf7d92f26dc03ca8c3
Matt Bessler <matthew.bessler@cix.csi.cuny.edu>
2018-04-14 14:16:18 -0400

    Parallelized counting sort

--- lab4.c
+++ lab4.c
@@ -9,85 +9,118 @@ void countingSort(int A[], int sortedA[], int, int);
 int main(int argc, char * argv[]){
 	
 	MPI_Init(&argc, &argv);
+	clock_t tStart = clock();
 	
-	int id, rank, i, root = 0;
-	int size = 10000;
+	int id, size, i, root = 0;
+	int n = 100000;
 	int max = 300;
-	int A[size], sortedArray[size], globalArray[size];
+	int A[n], sortedArray[n], globalArray[n];
 	
 	//The id of the processor
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	//Total number of processors
-	MPI_Comm_size(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
 	//Initialize array
 	if(id == 0){
-		for(i = 0; i < size; i++){
-			A[i] = rand() % 300;
+		for(i = 0; i < n; i++){
+			A[i] = rand() % max;
+			sortedArray[i] = 0;
+			globalArray[i] = 0;
+		}
+		
+		printf("Unsorted Array:\n");
+		for(i = 0; i < n; i++){
+			if(i != 0 && i % 10 == 0){
+				printf("%d\n", A[i]);
+			}
+			else{
+				printf("%d, ", A[i]);
+			}
 		}
 	}
 	
 	//The amount of elements to send to each processor
-	int sendCount = size / rank;
+	int sendCount = n / size;
 	int recvCount = sendCount;
-	int localA[sendCount], recvSortedArrays[rank];
+	int localA[sendCount], localSortedArray[sendCount];
+	
+	for(i = 0; i < sendCount; i++){
+		localA[i] = 0;
+		localSortedArray[i] = 0;
+	}
 	
 	//Evenly scattering the elements across all processors
 	MPI_Scatter(A, sendCount, MPI_INT, localA, recvCount, MPI_INT, root, MPI_COMM_WORLD);
 	
-	//Sort the arrays
-	int localSortedArray[recvCount];
 	countingSort(localA, localSortedArray, max, recvCount);
 	
-	if(rank == 0){
-		
-	}
+	//At this point the local array is sorted
 	
 	//Gather all sorted arrays
-	MPI_Gather(localSortedArray, recvCount, MPI_INT, recvSortedArrays[rank], recvCount, MPI_INT, root, MPI_COMM_WORLD);
+	MPI_Gather(localSortedArray, sendCount, MPI_INT, globalArray, recvCount, MPI_INT, root, MPI_COMM_WORLD);
 	
+	//Perform one last sort from gathered sorted-subarrays
 	if(id == 0){
-		for(i = 0; i 
+		//Last sort
+		countingSort(globalArray, sortedArray, max, n);
+		
+		printf("\n\nTime taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+		
+		//Print out sorted array
+		printf("\n\nSorted Array:\n");
+		for(i = 0; i < n; i++){
+			if(i != 0 && i % 10 == 0){
+				printf("%d\n", sortedArray[i]);
+			}
+			else{
+				printf("%d, ", sortedArray[i]);
+			}
+		}
 	}
 	
 	MPI_Finalize();
 }
 
-int findMaxElement(int A[], int size){
+int findMaxElement(int A[], int n){
 	int max = A[0];
 	int i = 0;
-	for(i = 1; i < size; i++){
+	for(i = 1; i < n; i++){
 		if(A[i] > max) max = A[i];
 	}
 	
 	return max;
 }
 
-void countingSort(int A[], int sortedA[], int max, int size){
+void countingSort(int A[], int sortedA[], int max, int n){
 	//This array provides temporary working storage
 	int C[max];
 	int i = 0;
-	for (i = 0; i < k; i++) {
+	for (i = 0; i < max; i++) {
 		C[i] = 0;
 	}
 
 	//{95, 1, 37, 27, 27}
 	//Counting the # of elements equal to each other in A
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < n; i++) {
 		C[A[i]] = C[A[i]] + 1;
 	}
 	//C[i] now contains the number of elements equal to i
 
 	//Count the # of elements less than or equal to i
-	for (i = 1; i < k; i++) {
+	for (i = 1; i < max; i++) {
+		if(id == 0){
+			if(i == 1) printf("%d - %d\n", 0, C[0]);
+			printf("%d - %d\n", i, C[i]);
+		}
 		C[i] = C[i] + C[i - 1];
 	}
 	//C[i] now contains the # of elements <= to i
 
 	//This is where the sorting happens
-	for (i = size-1; i >= 0; i--) {
+	for (i = n-1; i >= 0; i--) {
 		//Find the # of elements <= to value at index A[i]
-		sortedArray[C[A[i]]] = A[i];
+		sortedA[C[A[i]]] = A[i];
 		//Decrement the # of elements <= to value at index A[i} because it's been added to sorted array
 		C[A[i]] = C[A[i]] - 1;
 	}
